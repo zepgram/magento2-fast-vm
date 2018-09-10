a76f044881cdae06d3429c7c4c8c5bd2f43f7c68
Benjamin Calef <benjamin.calef@caudalie.com>
2018-09-10 23:52:10 +0200

    re-order execution

--- provision/120-magento-post.sh
+++ provision/120-magento-post.sh
@@ -16,31 +16,6 @@ magento deploy:mode:set "$PROJECT_MODE"
 magento config:set "admin/security/session_lifetime" "31536000"
 magento config:set "admin/security/lockout_threshold" "180"
 
-# Composer config
-if [ $PROJECT_SOURCE == "composer" ]; then
-  # Enable php ini
-  if [ -f "${PROJECT_PATH}/php.ini.sample" ]; then
-  	cp "$PROJECT_PATH"/php.ini.sample "$PROJECT_PATH"/php.ini
-  fi
-  # Run npm install if required files exist
-  if [ -f "${PROJECT_PATH}/Gruntfile.js.sample" ]; then
-  	cp "$PROJECT_PATH"/Gruntfile.js.sample "$PROJECT_PATH"/Gruntfile.js
-  fi
-  if [ -f "${PROJECT_PATH}/package.json.sample" ]; then
-  	cp "$PROJECT_PATH"/package.json.sample "$PROJECT_PATH"/package.json
-  fi
-  if [ -f "${PROJECT_PATH}/package.json" ] && [ -f "${PROJECT_PATH}/Gruntfile.js" ]; then
-  	cd "$PROJECT_PATH"; npm install &> /dev/null; npm update
-  fi
-else
-  if [ -f "${PROJECT_PATH}/.git/config" ]; then
-    # Git config (ignore permission change)
-    git --git-dir "$PROJECT_PATH"/.git config user.name "$PROJECT_USER_NAME"
-    git --git-dir "$PROJECT_PATH"/.git config user.email "$PROJECT_USER_EMAIL"
-    git --git-dir "$PROJECT_PATH"/.git config core.filemode false
-  fi
-fi
-
 # Redis configuration
 magento setup:config:set \
       --cache-backend=redis \
@@ -58,6 +33,31 @@ magento setup:config:set \
       --session-save-redis-host=127.0.0.1 \
       --session-save-redis-port=6379 \
       --session-save-redis-db=2
+
+# Composer config
+if [ $PROJECT_SOURCE == "composer" ]; then
+  # Enable php ini
+  if [ -f "${PROJECT_PATH}/php.ini.sample" ]; then
+    cp "$PROJECT_PATH"/php.ini.sample "$PROJECT_PATH"/php.ini
+  fi
+  # Run npm install if required files exist
+  if [ -f "${PROJECT_PATH}/Gruntfile.js.sample" ]; then
+    cp "$PROJECT_PATH"/Gruntfile.js.sample "$PROJECT_PATH"/Gruntfile.js
+  fi
+  if [ -f "${PROJECT_PATH}/package.json.sample" ]; then
+    cp "$PROJECT_PATH"/package.json.sample "$PROJECT_PATH"/package.json
+  fi
+  if [ -f "${PROJECT_PATH}/package.json" ] && [ -f "${PROJECT_PATH}/Gruntfile.js" ]; then
+    cd "$PROJECT_PATH"; npm install &> /dev/null; npm update
+  fi
+else
+  if [ -f "${PROJECT_PATH}/.git/config" ]; then
+    # Git config (ignore permission change)
+    git --git-dir "$PROJECT_PATH"/.git config user.name "$PROJECT_USER_NAME"
+    git --git-dir "$PROJECT_PATH"/.git config user.email "$PROJECT_USER_EMAIL"
+    git --git-dir "$PROJECT_PATH"/.git config core.filemode false
+  fi
+fi
 
 # Extra post-build
 if [ -f /home/vagrant/provision/120-post-build.sh ]; then
