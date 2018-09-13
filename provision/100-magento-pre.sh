7cc3d296a2c17d1a97e00f876592b6962e22e505
Benjamin Calef <benjamin.calef@caudalie.com>
2018-09-13 12:17:33 +0200

    add setup for nfs mount

--- provision/100-magento-pre.sh
+++ provision/100-magento-pre.sh
@@ -41,7 +41,7 @@ echo 'Applying permissions to $PROJECT_PATH project'
 cd "$PROJECT_PATH" \\
 && sudo find var vendor pub/static pub/media app/etc -type f -exec chmod g+w {} \; \\
 && sudo find var vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} \; \\
-&& sudo chmod u+x bin/magento
+&& sudo chmod u+x bin/magento \\
 && sudo chown -R $PROJECT_USER:www-data . &> /dev/null
 fi
 EOF
